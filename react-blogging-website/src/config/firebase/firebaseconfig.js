e1061ed47a784ca9e6c374e6920ed25346449918
AnasAhmed001 <hafizanasahmed8@gmail.com>
2024-09-24 20:11:56 +0500

    app initialized

---
+++ react-blogging-website/src/config/firebase/firebaseconfig.js
@@ -0,0 +1,14 @@
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBhaEt2rj8Sq3WopNjR6o_X_-ewCpmYl9g",
+  authDomain: "blogging-website-react.firebaseapp.com",
+  projectId: "blogging-website-react",
+  storageBucket: "blogging-website-react.appspot.com",
+  messagingSenderId: "1097368988409",
+  appId: "1:1097368988409:web:a08b12e30416ed26339028",
+  measurementId: "G-YG19CSC1WS"
+};
+
+const app = initializeApp(firebaseConfig);
